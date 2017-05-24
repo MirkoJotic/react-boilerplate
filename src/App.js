45e8e37ebac764148e2f29d82feb04f94c3a377c
mirko91 <joticmirko@gmail.com>
2017-05-24 10:09:22 -0400

    adding esling config with react plugin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import HelloWorld from './components/HelloWorld'
+import CountDown from './components/CountDown'
 
 const App = () => (
     <div className="container">
         <HelloWorld/>
+        <CountDown/>
     </div>
 )
 
